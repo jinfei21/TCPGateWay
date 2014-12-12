88ec3b3550e71fc68b227727a084fe5ea0cd15ed
jinfei21 <63581560@qq.com>
2014-12-12 22:26:23 +0800

    add foo

--- src/main/java/com/ctrip/gateway/client/DefaultGateClient.java
+++ src/main/java/com/ctrip/gateway/client/DefaultGateClient.java
@@ -1,6 +1,9 @@
 package com.ctrip.gateway.client;
 
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,22 +14,22 @@ public class DefaultGateClient implements GateClient {
 	
 	private static final Logger logger = LoggerFactory.getLogger(DefaultGateClient.class);
 	
-	
-	
+	private Map<String,GateConnectionPool> pool;
 	
 	public DefaultGateClient(){
-		
+		this.pool = new ConcurrentHashMap<String,GateConnectionPool>();
 	}
 
-	public GateResponse request(String host, int port, GateRequest request)
-			throws Exception {
-		// TODO Auto-generated method stub
+	public GateResponse request(String host, int port, GateRequest request)throws Exception {
+		
+		
 		return null;
 	}
 
-	public GateResponse request(String host, int port, GateRequest request,
-			int timeout) throws Exception {
-		// TODO Auto-generated method stub
+	public GateResponse request(String host, int port, GateRequest request,int timeout) throws Exception {
+
+
+		
 		return null;
 	}
 
