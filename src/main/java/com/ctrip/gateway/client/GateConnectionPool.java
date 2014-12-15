115680bb3c2834846e468c531bbfc6644bb3009c
yjfei <yjfei@ctrip.com>
2014-12-15 11:12:39 +0800

    no message

--- src/main/java/com/ctrip/gateway/client/GateConnectionPool.java
+++ src/main/java/com/ctrip/gateway/client/GateConnectionPool.java
@@ -1,11 +1,21 @@
 package com.ctrip.gateway.client;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class GateConnectionPool {
 
 	private volatile int minCount;
 	private volatile int maxCount;
 	
-	public GateConnectionPool(){
+	private ReentrantLock lock = new ReentrantLock();
+	private Condition condition = lock.newCondition();
+	
+	
+	
+	public GateConnectionPool(int minCount,int maxCount){
 		
 	}
+	
+	
 }
